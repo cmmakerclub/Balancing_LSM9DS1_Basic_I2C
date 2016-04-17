43cc78c4eeafc1b7c686ea10cc0cc79a69c9f4b4
Nat Weerawan <nat.wrw@gmail.com>
2016-04-17 21:19:54 +0700

    Add balancing code.

--- LSM9DS1_Basic_I2C.ino
+++ LSM9DS1_Basic_I2C.ino
@@ -1,68 +1,24 @@
 #include <Arduino.h>
-
-/*****************************************************************
-LSM9DS1_Basic_I2C.ino
-SFE_LSM9DS1 Library Simple Example Code - I2C Interface
-Jim Lindblom @ SparkFun Electronics
-Original Creation Date: April 30, 2015
-https://github.com/sparkfun/LSM9DS1_Breakout
-
+/*
 The LSM9DS1 is a versatile 9DOF sensor. It has a built-in
-accelerometer, gyroscope, and magnetometer. Very cool! Plus it
-functions over either SPI or I2C.
-
-This Arduino sketch is a demo of the simple side of the
 SFE_LSM9DS1 library. It'll demo the following:
 * How to create a LSM9DS1 object, using a constructor (global
   variables section).
 * How to use the begin() function of the LSM9DS1 class.
 * How to read the gyroscope, accelerometer, and magnetometer
-  using the readGryo(), readAccel(), readMag() functions and 
+  using the readGryo(), readAccel(), readMag() functions and
   the gx, gy, gz, ax, ay, az, mx, my, and mz variables.
-* How to calculate actual acceleration, rotation speed, 
-  magnetic field strength using the calcAccel(), calcGyro() 
+* How to calculate actual acceleration, rotation speed,
+  magnetic field strength using the calcAccel(), calcGyro()
   and calcMag() functions.
-* How to use the data from the LSM9DS1 to calculate 
+* How to use the data from the LSM9DS1 to calculate
   orientation and heading.
+  */
 
-Hardware setup: This library supports communicating with the
-LSM9DS1 over either I2C or SPI. This example demonstrates how
-to use I2C. The pin-out is as follows:
-	LSM9DS1 --------- Arduino
-	 SCL ---------- SCL (A5 on older 'Duinos')
-	 SDA ---------- SDA (A4 on older 'Duinos')
-	 VDD ------------- 3.3V
-	 GND ------------- GND
-(CSG, CSXM, SDOG, and SDOXM should all be pulled high. 
-Jumpers on the breakout board will do this for you.)
-
-The LSM9DS1 has a maximum voltage of 3.6V. Make sure you power it
-off the 3.3V rail! I2C pins are open-drain, so you'll be 
-(mostly) safe connecting the LSM9DS1's SCL and SDA pins 
-directly to the Arduino.
-
-Development environment specifics:
-	IDE: Arduino 1.6.3
-	Hardware Platform: SparkFun Redboard
-	LSM9DS1 Breakout Version: 1.0
-
-This code is beerware. If you see me (or any other SparkFun 
-employee) at the local, and you've found our code helpful, 
-please buy us a round!
-
-Distributed as-is; no warranty is given.
-*****************************************************************/
-// The SFE_LSM9DS1 library requires both Wire and SPI be
-// included BEFORE including the 9DS1 library.
 #include <Wire.h>
 #include <SPI.h>
 #include <SparkFunLSM9DS1.h>
 
-//////////////////////////
-// LSM9DS1 Library Init //
-//////////////////////////
-// Use the LSM9DS1 class to create an object. [imu] can be
-// named anything, we'll refer to that throught the sketch.
 LSM9DS1 imu;
 
 ///////////////////////
@@ -75,32 +31,103 @@ LSM9DS1 imu;
 ////////////////////////////
 // Sketch Output Settings //
 ////////////////////////////
-#define PRINT_CALCULATED
-//#define PRINT_RAW
-#define PRINT_SPEED 250 // 250 ms between prints
 
-// Earth's magnetic field varies by location. Add or subtract 
-// a declination to get a more accurate heading. Calculate 
-// your's here:
-// http://www.ngdc.noaa.gov/geomag-web/#declination
-#define DECLINATION -8.58 // Declination (degrees) in Boulder, CO.
+//#define ACCELEROMETER_SENSITIVITY 8192.0
+#define GYROSCOPE_SENSITIVITY 16.348
+// #define GYROSCOPE_SENSITIVITY 65.536
+// 2000*2/2^16
+
+#define M_PI 3.14159265359
+#define dt (10.0/1000.0)             // 100hz = 10ms
+
+#define RMotor_offset 40 // The offset of the Motor
+#define LMotor_offset 50 // The offset of the Motor
+
+// walkin
+// static float kp = 40.00f;
+// static float kd = 1000.00f;
+
+// static float kp = 40.00f;
+// static float kd = 600.00f;
+
+static float kp = 23.00f;
+static float kd = 950.00f;
+
+static float ki = 0.7f;
+
+uint32_t _prev = millis();
+
+static uint32_t lastTime = millis();
+
+// int TN1 = 4;
+int TN1 = 10;
+int TN2 = 3;
+int ENA = 5;
+int TN3 = 8;
+// int TN4 = 7;
+int TN4 = 9;
+int ENB = 6;
+
+static float error = 0;  // Proportion
+static float errSum = 0;
+static float dErr = 0;
+
+struct Motor {
+  float LOutput;
+  float ROutput;
+} motor_t;
 
 
-void printAttitude(float ax, float ay, float az, float mx, float my, float mz);
-void setup() 
+//http://www.pieter-jan.com
+static void ComplementaryFilter(short accData[3], short gyrData[3], float *pitch, float *roll)
 {
-  
+  float pitchAcc, rollAcc;
+
+  // Integrate the gyroscope data -> int(angularSpeed) = angle
+  *pitch += ((float)gyrData[0] / GYROSCOPE_SENSITIVITY) * dt; // Angle around the X-axis
+  *roll -= ((float)gyrData[1] / GYROSCOPE_SENSITIVITY) * dt;    // Angle around the Y-axis
+
+  // Compensate for drift with accelerometer data if !bullshit
+  // Sensitivity = -2 to 2 G at 16Bit -> 2G = 32768 && 0.5G = 8192
+  int forceMagnitudeApprox = abs(accData[0]) + abs(accData[1]) + abs(accData[2]);
+  if (forceMagnitudeApprox > 8192 && forceMagnitudeApprox < 32768)
+  {
+    // Turning around the X axis results in a vector on the Y-axis
+    pitchAcc = atan2f((float)accData[1], (float)accData[2]) * 180 / M_PI;
+    *pitch = *pitch * 0.98 + pitchAcc * 0.02;
+
+    // Turning around the Y axis results in a vector on the X-axis
+    rollAcc = atan2f((float)accData[0], (float)accData[2]) * 180 / M_PI;
+    *roll = *roll * 0.98 + rollAcc * 0.02;
+  }
+}
+
+void PWMControl(float LOutput, float ROutput);
+void myPID(float filtered_angle, struct Motor *motor);
+void readIMUSensor(float*);
+
+
+void setup()
+{
+  // CurieImu.setFullScaleAccelRange(BMI160_ACCEL_RANGE_2G);
+  // CurieImu.setFullScaleGyroRange(BMI160_GYRO_RANGE_2000);
   Serial.begin(115200);
-  
+
   // Before initializing the IMU, there are a few settings
   // we may need to adjust. Use the settings struct to set
   // the device's communication mode and addresses:
+
   imu.settings.device.commInterface = IMU_MODE_I2C;
   imu.settings.device.mAddress = LSM9DS1_M;
   imu.settings.device.agAddress = LSM9DS1_AG;
-  // The above lines will only take effect AFTER calling
-  // imu.begin(), which verifies communication with the IMU
-  // and turns it on.
+
+  imu.settings.accel.scale = A_SCALE_2G;
+  imu.settings.accel.sampleRate = 4;
+
+  imu.settings.gyro.scale = G_SCALE_2000DPS;
+  imu.settings.gyro.sampleRate = 4;
+
+  Serial.println("START IMU");
   if (!imu.begin())
   {
     Serial.println("Failed to communicate with LSM9DS1.");
@@ -112,143 +139,117 @@ void setup()
     while (1)
       ;
   }
+  Serial.println("FINISHED");
+
+  // the following calibration procedure to work correctly!
+  Serial.print("Starting Gyroscope calibration...");
+  imu.calibrate(true);
+
+  Serial.println(" Done");
+  pinMode(TN1, OUTPUT);
+  pinMode(TN2, OUTPUT);
+  pinMode(TN3, OUTPUT);
+  pinMode(TN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+   _prev = millis();
 }
 
-void loop()
-{
-  printGyro();  // Print "G: gx, gy, gz"
-  printAccel(); // Print "A: ax, ay, az"
-  printMag();   // Print "M: mx, my, mz"
-  
-  // Print the heading and orientation for fun!
-  // Call print attitude. The LSM9DS1's magnetometer x and y
-  // axes are opposite to the accelerometer, so my and mx are
-  // substituted for each other.
-  printAttitude(imu.ax, imu.ay, imu.az, -imu.my, -imu.mx, imu.mz);
-  Serial.println();
-  
-  delay(PRINT_SPEED);
+void loop() {
+  static Motor motor;
+  static float pitch_value_filtered;
+  if (millis() - _prev > dt * 1000 ) {
+    _prev = millis();
+    //   If angle > 45 or < -45 then stop the robot
+    readIMUSensor(&pitch_value_filtered);
+    if (abs(pitch_value_filtered) < 45) {
+      myPID(pitch_value_filtered, &motor);
+      PWMControl(motor.LOutput, motor.ROutput);
+    }
+    else
+    {
+      //TODO:// set zero when robot down.
+          //  Output = error = errSum = dErr = 0;
+      // Serial.println("STOP");
+      analogWrite(ENA, 0);  // Stop the wheels
+      analogWrite(ENB, 0);  // Stop the wheels
+    }
+  }
 }
 
-void printGyro()
-{
-  // To read from the gyroscope, you must first call the
-  // readGyro() function. When this exits, it'll update the
-  // gx, gy, and gz variables with the most current data.
-  imu.readGyro();
-  
-  // Now we can use the gx, gy, and gz variables as we please.
-  // Either print them as raw ADC values, or calculated in DPS.
-  Serial.print("G: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcGyro helper function to convert a raw ADC value to
-  // DPS. Give the function the value that you want to convert.
-  Serial.print(imu.calcGyro(imu.gx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gy), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcGyro(imu.gz), 2);
-  Serial.println(" deg/s");
-#elif defined PRINT_RAW
-  Serial.print(imu.gx);
-  Serial.print(", ");
-  Serial.print(imu.gy);
-  Serial.print(", ");
-  Serial.println(imu.gz);
-#endif
-}
+void myPID(float filtered_angle, struct Motor *motor) {
+  static float lastErr = 0;
+  uint32_t timeChange = (millis() - lastTime);
+  static float Output;
+  lastTime = millis();
 
-void printAccel()
-{
-  // To read from the accelerometer, you must first call the
-  // readAccel() function. When this exits, it'll update the
-  // ax, ay, and az variables with the most current data.
-  imu.readAccel();
-  
-  // Now we can use the ax, ay, and az variables as we please.
-  // Either print them as raw ADC values, or calculated in g's.
-  Serial.print("A: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcAccel helper function to convert a raw ADC value to
-  // g's. Give the function the value that you want to convert.
-  Serial.print(imu.calcAccel(imu.ax), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.ay), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcAccel(imu.az), 2);
-  Serial.println(" g");
-#elif defined PRINT_RAW 
-  Serial.print(imu.ax);
-  Serial.print(", ");
-  Serial.print(imu.ay);
-  Serial.print(", ");
-  Serial.println(imu.az);
-#endif
+  error = filtered_angle;  // Proportion
+  errSum += error * timeChange;  // Integration
+  dErr = (error - lastErr) / timeChange;  // Differentiation
+  Output = kp * error + ki * errSum + kd * dErr;
+  lastErr = error;
+
+  motor->LOutput = Output;
+  motor->ROutput = Output;
 
 }
 
-void printMag()
+void PWMControl(float LOutput, float ROutput)
 {
-  // To read from the magnetometer, you must first call the
-  // readMag() function. When this exits, it'll update the
-  // mx, my, and mz variables with the most current data.
-  imu.readMag();
-  
-  // Now we can use the mx, my, and mz variables as we please.
-  // Either print them as raw ADC values, or calculated in Gauss.
-  Serial.print("M: ");
-#ifdef PRINT_CALCULATED
-  // If you want to print calculated values, you can use the
-  // calcMag helper function to convert a raw ADC value to
-  // Gauss. Give the function the value that you want to convert.
-  Serial.print(imu.calcMag(imu.mx), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.my), 2);
-  Serial.print(", ");
-  Serial.print(imu.calcMag(imu.mz), 2);
-  Serial.println(" gauss");
-#elif defined PRINT_RAW
-  Serial.print(imu.mx);
-  Serial.print(", ");
-  Serial.print(imu.my);
-  Serial.print(", ");
-  Serial.println(imu.mz);
-#endif
-}
-
-// Calculate pitch, roll, and heading.
-// Pitch/roll calculations take from this app note:
-// http://cache.freescale.com/files/sensors/doc/app_note/AN3461.pdf?fpsp=1
-// Heading calculations taken from this app note:
-// http://www51.honeywell.com/aero/common/documents/myaerospacecatalog-documents/Defense_Brochures-documents/Magnetic__Literature_Application_notes-documents/AN203_Compass_Heading_Using_Magnetometers.pdf
-void printAttitude(
-float ax, float ay, float az, float mx, float my, float mz)
-{
-  float roll = atan2(ay, az);
-  float pitch = atan2(-ax, sqrt(ay * ay + az * az));
-  
-  float heading;
-  if (my == 0)
-    heading = (mx < 0) ? 180.0 : 0;
+  if (LOutput > 0)
+  {
+    digitalWrite(TN1, 0);
+    digitalWrite(TN2, 1);
+  }
+  else if (LOutput < 0)
+  {
+    digitalWrite(TN1, 1);
+    digitalWrite(TN2, 0);
+  }
   else
-    heading = atan2(mx, my);
-    
-  heading -= DECLINATION * PI / 180;
-  
-  if (heading > PI) heading -= (2 * PI);
-  else if (heading < -PI) heading += (2 * PI);
-  else if (heading < 0) heading += 2 * PI;
-  
-  // Convert everything from radians to degrees:
-  heading *= 180.0 / PI;
-  pitch *= 180.0 / PI;
-  roll  *= 180.0 / PI;
-  
-  Serial.print("Pitch, Roll: ");
-  Serial.print(pitch, 2);
-  Serial.print(", ");
-  Serial.println(roll, 2);
-  Serial.print("Heading: "); Serial.println(heading, 2);
+  {
+    digitalWrite(ENA, 0);
+  }
+
+  if (ROutput > 0)
+  {
+    digitalWrite(TN3, 1);
+    digitalWrite(TN4, 0);
+  }
+  else if (ROutput < 0)
+  {
+    digitalWrite(TN3, 0);
+    digitalWrite(TN4, 1);
+  }
+  else
+  {
+    digitalWrite(ENB, 0);
+  }
+
+  analogWrite(ENA, min(255, (abs(LOutput) + LMotor_offset)));
+  analogWrite(ENB, min(255, (abs(ROutput) + RMotor_offset)));
+}
+
+void readIMUSensor(float *pitch_value_filtered) {
+  static float roll;
+  static float pitch;
+  // put your main code here, to run repeatedly:
+  short accData[3];
+  short gyrData[3];
+
+  // CurieImu.getMotion6(&accData[0], &accData[1], &accData[2], &gyrData[0], &gyrData[1], &gyrData[2]);
+  imu.readAccel();
+  imu.readGyro();
+  accData[0] = imu.ax;
+  accData[1] = imu.ay;
+  accData[2] = imu.az;
+
+  gyrData[0] = imu.gx;
+  gyrData[1] = imu.gy;
+  gyrData[2] = imu.gz;
+
+
+  ComplementaryFilter(accData, gyrData, &pitch, &roll);
+
+  *pitch_value_filtered = pitch;
 }
